98c121ab4bfcd71686b79b06487ac8ddea5dac5f
lukasz <lorzechowski@protonmail.com>
2019-05-29 21:56:00 +0200

    coinstable update table deleted

--- src/components/Tables/CoinTable/CoinsTable.js
+++ src/components/Tables/CoinTable/CoinsTable.js
@@ -1,91 +1,57 @@
-import React, { Component } from "react";
-import Table from "../Table/Table";
+import React from "react";
+
 import { Link } from "react-router-dom";
 import SortButton from "../../UI/Buton/TableSortButton/TableSortButton";
 import style from "./CoinsTable.module.css";
 
-class coinsTable extends Component {
-  state = {
-    coinsData: [],
-    headers: [
-      "#",
-      "Name",
-      "Price USD",
-      "Price change %",
-      "Volume M USD",
-      "Market cap %",
-      "Marketcap M USD"
-    ]
-  };
-  componentDidMount() {
-    const coinsData = [];
+const coinsTable =props => {
 
-    for (let coin of this.props.coins) {
-      const data = {
-        rank: coin.rank,
-        name: coin.name,
-        price: coin.quotes.USD.price.toFixed(2),
-        percentChange: coin.quotes.USD.percent_change_24h,
-        volume24h: (coin.quotes.USD.volume_24h / 100000).toFixed(3),
-        marketCapContribution: (
-          (coin.quotes.USD.market_cap / this.props.global.market_cap_usd) *
-          100
-        ).toFixed(3),
-        marketCapUSD: (coin.quotes.USD.market_cap / 1000000).toFixed(2)
-      };
-      coinsData.push(data);
-    }
-    console.log(coinsData);
-  }
+  
 
-  coins = this.setCoins();
-  headers = (
+
+ const coins = setCoins();
+  const headers = (
     <tr className={style.table__row}>
       <th
         className={style.table__button}
-        onClick={() => this.props.sort("rank")}
+        onClick={() => props.sort("rank")}
       >
         #
       </th>
       <th>
-        <SortButton name={"Name"} sort={() => this.props.sort("name")} />
+        <SortButton name={"Name"} sort={() => props.sort("name")} />
       </th>
       <th>
         <SortButton
           name={"Price USD"}
-          sort={() => this.props.sort("quotes.USD.price")}
+          sort={() => props.sort("quotes.USD.price")}
         />
       </th>
       <th>
         <SortButton
           name={"Price change"}
-          sort={() => this.props.sort("quotes.USD.percent_change_24h")}
+          sort={() => props.sort("quotes.USD.percent_change_24h")}
         />
       </th>
       <th>
         <SortButton
           name={"Volume M USD"}
-          sort={() => this.props.sort("quotes.USD.volume_24h")}
+          sort={() => props.sort("quotes.USD.volume_24h")}
         />
       </th>
-      <th>
-        <SortButton
-          name={"Market Cap %"}
-          sort={() => this.props.sort("quotes.USD.volume_24h")}
-        />
-      </th>
+
       <th>
         <SortButton
           name={"Market cap M USD"}
-          sort={() => this.props.sort("quotes.USD.market_cap")}
+          sort={() => props.sort("quotes.USD.market_cap")}
         />
       </th>
     </tr>
   );
-  setCoins() {
+ function setCoins() {
     const tab = [];
 
-    for (let coin of this.props.coins) {
+    for (let coin of props.coins) {
       const row = (
         <tr key={coin.id}>
           <td>{coin.rank}</td>
@@ -95,12 +61,7 @@ class coinsTable extends Component {
           <td>{coin.quotes.USD.price.toFixed(2)}</td>
           <td>{coin.quotes.USD.percent_change_24h}%</td>
           <td>{(coin.quotes.USD.volume_24h / 100000).toFixed(3)}</td>
-          <td>
-            {(
-              (coin.quotes.USD.market_cap / this.props.global.market_cap_usd) *
-              100
-            ).toFixed(3)}
-          </td>
+    
           <td>{(coin.quotes.USD.market_cap / 1000000).toFixed(2)}</td>
         </tr>
       );
@@ -111,10 +72,12 @@ class coinsTable extends Component {
     }
     return tab;
   }
-  render() {
+
     return (
-      <Table className={style.table} headers={this.headers} data={this.coins} />
+      <table >
+      <thead >{headers}</thead>
+      <tbody>{coins}</tbody>
+    </table>
     );
-  }
 }
 export default coinsTable;
