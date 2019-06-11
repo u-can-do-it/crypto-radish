5b493b279434b7e11535930f9238e98ed4acb13a
lukasz <lorzechowski@protonmail.com>
2019-06-11 19:27:24 +0200

    build

---
+++ build/precache-manifest.3007f857f3c4a39325999850023e31a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c8911694ff632bead51929425e084a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c13b8c71c90c7b33fef9",
+    "url": "/static/css/main.a7de24b1.chunk.css"
+  },
+  {
+    "revision": "b0cca40cc9cd7d2edbf6",
+    "url": "/static/js/2.55180c1e.chunk.js"
+  },
+  {
+    "revision": "c13b8c71c90c7b33fef9",
+    "url": "/static/js/main.d1f0be2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
